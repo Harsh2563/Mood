cf5d226a353463a8c463d0fcb1053c63ba15e821
Harsh Rai <htc38199@gmail.com>
2023-12-19 23:23:28 +0530

    got entry content on particular entrypage

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,6 +1,26 @@
-const EntryPage = ({params})=> {
+import Entry from "@/components/Entry"
+import { getUserFromClerkId } from "@/utils/auth"
+import { prisma } from "@/utils/db"
+
+const userEntry = async(id)=> {
+    const user = await getUserFromClerkId();
+    const requiredEntry = await prisma.journalEntry.findUnique({
+        where:{
+            userId_id: {
+                userId: user.id,
+                id,
+            },
+        },
+    })
+    return requiredEntry;
+}
+
+const EntryPage = async ({params})=> {
+    const entry = await userEntry(params.id)
     return (
-        <div>{params.id}</div>
+        <div>
+            <Entry entry={entry}/>
+        </div>
     )
 }
 
---
+++ components/Entry.tsx
@@ -0,0 +1,7 @@
+const Entry = ({entry})=> {
+   return (
+    <div>{entry.content}</div>
+   )
+} 
+
+export default Entry;
